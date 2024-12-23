d92b8e23e0c253127220924c6bd1b75eb051e2ed
Slaviša Pokimica <slavisa.pokimica@anywhere365.net>
2024-12-23 10:22:10 +0100

    adding custom config

--- PokeTestWebApp/Pages/Index.cshtml.cs
+++ PokeTestWebApp/Pages/Index.cshtml.cs
@@ -6,15 +6,18 @@ namespace PokeTestWebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public void OnGet()
         {
             _logger.LogInformation("MY LOG MESSAGE");
+            ViewData["MyConfig"] = _config["MyConfig"];
         }
     }
 }
